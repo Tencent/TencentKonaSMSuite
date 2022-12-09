09454324f4ab44b0222a9e575a432d4f9693a50e
John Jiang <johnsjiang@tencent.com>
2022-12-09 09:05:23 +0800

    TKSS-100: Backport JDK-8295010: Reduce if required in EC limbs operations

--- kona-crypto/src/main/java/com/tencent/kona/sun/security/util/math/MutableIntegerModuloP.java
+++ kona-crypto/src/main/java/com/tencent/kona/sun/security/util/math/MutableIntegerModuloP.java
@@ -150,12 +150,4 @@ public interface MutableIntegerModuloP extends IntegerModuloP {
      * @return this
      */
     MutableIntegerModuloP setAdditiveInverse();
-
-    /**
-     * Some implementations required reduction operations to be requested
-     * by the client at certain times. This method reduces the representation.
-     *
-     * @return this
-     */
-    MutableIntegerModuloP setReduced();
 }
