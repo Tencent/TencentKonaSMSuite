5b96e5cf91d4f1c36f94ef66d558f1940af91bda
John Jiang <johnsjiang@tencent.com>
2023-07-25 10:56:10 +0800

    TKSS-319: KeyStoreUtil::getKeyStore should release resource

--- kona-pkix/src/main/java/com/tencent/kona/sun/security/tools/KeyStoreUtil.java
+++ kona-pkix/src/main/java/com/tencent/kona/sun/security/tools/KeyStoreUtil.java
@@ -30,6 +30,7 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import java.io.StreamTokenizer;
@@ -294,7 +295,9 @@ public class KeyStoreUtil {
             CertificateException {
         String type = keyStoreType(file);
         KeyStore keyStore = PKIXInsts.getKeyStore(type);
-        keyStore.load(new FileInputStream(file), password);
+        try (InputStream in = new FileInputStream(file)) {
+            keyStore.load(in, password);
+        }
         return keyStore;
     }
 
