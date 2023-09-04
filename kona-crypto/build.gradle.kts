7b4ee2f403e6212df21144789bc6cdf0e4505779
John Jiang <johnsjiang@tencent.com>
2023-09-04 18:46:10 +0800

    TKSS-377: Declare junit-platform-launcher as runtime dependency explicitly

--- kona-crypto/build.gradle.kts
+++ kona-crypto/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
 
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
 
     jmhImplementation(libs.jmh.core)
     jmhAnnotationProcessor(libs.jmh.generator.annprocess)
--- kona-pkix/build.gradle.kts
+++ kona-pkix/build.gradle.kts
@@ -7,4 +7,5 @@ dependencies {
 
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
