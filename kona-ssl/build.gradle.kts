dca541e27ec25faa0d66ce3f01e68e6964b41422
John Jiang <johnsjiang@tencent.com>
2023-09-13 17:24:21 +0800

    TKSS-388: Add TLS demo with gRPC

--- kona-ssl/build.gradle.kts
+++ kona-ssl/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     id("kona-common")
+    alias(libs.plugins.protobuf).apply(true)
 }
 
 dependencies {
@@ -14,6 +17,8 @@ dependencies {
     testImplementation(libs.apache.httpclient)
     testImplementation(libs.okhttp)
 
+    testImplementation(libs.bundles.grpc)
+
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
     testRuntimeOnly(libs.junit.platform.launcher)
@@ -21,4 +26,24 @@ dependencies {
     jmhImplementation(libs.jmh.core)
     jmhImplementation(libs.jmh.generator.annprocess)
     jmhImplementation(sourceSets["test"].runtimeClasspath)
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protoc.get().toString()
+    }
+
+    plugins {
+        id("grpc") {
+            artifact = libs.grpc.protoc.gen.grpc.get().toString()
+        }
+    }
+
+    generateProtoTasks {
+         all().forEach {
+            it.plugins {
+                id("grpc")
+            }
+        }
+    }
 }
