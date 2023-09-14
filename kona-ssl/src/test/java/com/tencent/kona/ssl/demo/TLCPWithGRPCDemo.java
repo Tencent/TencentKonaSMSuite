f523a0a713d41e8f66cc48325616c7ae50e6a8f0
John Jiang <johnsjiang@tencent.com>
2023-09-14 10:41:46 +0800

    TKSS-390: Add TLCP demo with gRPC

--- kona-ssl/src/test/java/com/tencent/kona/ssl/demo/TLCPWithGRPCDemo.java
+++ kona-ssl/src/test/java/com/tencent/kona/ssl/demo/TLCPWithGRPCDemo.java
@@ -39,10 +39,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.StreamSupport;
 
 /**
- * A comprehensive demo for TLS 1.3 connection with RFC 8998-defined
- * cipher suite, named group and signature scheme.
+ * A comprehensive demo for TLCP with gRPC.
  */
-public class TLSWithGRPCDemo {
+public class TLCPWithGRPCDemo {
 
     /* The CA certificate.
      *
@@ -50,125 +49,187 @@ public class TLSWithGRPCDemo {
      *     Data:
      *         Version: 3 (0x2)
      *         Serial Number:
-     *             66:b3:f0:17:73:ea:58:60:dc:ae:15:5b:7b:ca:d7:e0:3c:09:98:45
+     *             21:9c:f4:c5:db:bb:03:3e:69:d2:32:6a:46:77:aa:f7:a3:6b:68:52
      *         Signature Algorithm: sm3WithSM2Sign
-     *         Issuer: CN = ca-sm2sm2
+     *         Issuer: CN = tlcp-ca
      *         Validity
-     *             Not Before: Sep 11 20:15:16 2021 GMT
-     *             Not After : Sep  9 20:15:16 2031 GMT
-     *         Subject: CN = intca-sm2sm2-sm2sm2
+     *             Not Before: Mar 10 11:08:29 2022 GMT
+     *             Not After : Mar  7 11:08:29 2032 GMT
+     *         Subject: CN = tlcp-intca
      *         Subject Public Key Info:
      *             Public Key Algorithm: id-ecPublicKey
      *                 Public-Key: (256 bit)
      *                 pub:
-     *                     04:62:d7:db:de:87:3d:46:51:c4:25:00:d3:0b:68:
-     *                     ed:f9:64:ff:e4:62:9b:80:85:fb:6a:f8:c3:3f:4b:
-     *                     38:e5:72:5c:92:b8:d8:33:0f:9f:50:da:53:93:52:
-     *                     06:d2:e6:90:f2:ae:55:c8:c1:40:4f:50:17:a4:b8:
-     *                     e2:7b:ba:8f:64
+     *                     04:bf:d8:1f:2c:1f:47:4f:0d:e6:04:ab:14:73:4f:
+     *                     75:88:44:5d:ec:59:25:3d:9b:8c:74:33:1d:f4:66:
+     *                     c6:97:1a:db:41:21:4a:ab:34:75:7d:e8:01:03:78:
+     *                     8f:d8:e7:c9:cd:8f:fd:fe:45:82:d8:02:d0:53:2a:
+     *                     cb:d0:e4:c8:61
      *                 ASN1 OID: SM2
      *         X509v3 extensions:
      *             X509v3 Subject Key Identifier:
-     *                 BD:AA:64:6D:4D:40:33:81:B7:50:B3:4D:2F:12:7D:8E:A6:EF:64:42
+     *                 CB:67:1D:38:EC:4A:66:5D:98:76:4D:71:5F:47:7D:43:3E:59:07:FD
      *             X509v3 Authority Key Identifier:
-     *                 keyid:0A:61:C1:22:D6:20:06:80:48:68:8A:96:EF:08:47:75:FF:32:01:84
+     *                 keyid:5A:6A:53:54:DC:B7:80:2A:6B:56:C2:37:F8:18:F2:81:A0:81:B3:4D
      *
      *             X509v3 Basic Constraints: critical
      *                 CA:TRUE
      *             X509v3 Key Usage: critical
-     *                 Digital Signature, Certificate Sign, CRL Sign
+     *                 Digital Signature, Key Encipherment, Data Encipherment, Key Agreement, Certificate Sign, CRL Sign
      *             X509v3 Extended Key Usage: critical
-     *                 OCSP Signing
+     *                 OCSP Signing, Time Stamping
      *     Signature Algorithm: sm3WithSM2Sign
-     *          30:45:02:20:61:67:6c:27:97:6b:b2:29:bb:d4:d9:d2:4b:df:
-     *          51:75:e9:a4:8c:4d:6a:47:dd:5a:80:87:0e:e2:3e:65:ca:3c:
-     *          02:21:00:eb:87:26:69:1c:14:0b:ff:43:32:16:1d:bd:21:b3:
-     *          b5:8d:fe:dd:ec:ad:d7:60:ff:c2:43:77:87:9e:63:b4:16
+     *          30:45:02:21:00:98:ea:df:4b:50:eb:8d:f8:19:9e:32:15:11:
+     *          68:01:26:8d:f0:a8:ed:5e:28:a1:f4:14:d9:ed:31:2c:7f:61:
+     *          36:02:20:77:64:87:fe:aa:50:01:e2:1c:ec:e0:d7:d1:8e:1a:
+     *          e5:5f:5e:f6:48:0a:60:3c:7c:68:1d:4c:5e:81:fd:d2:5e
      */
     private static final String CA =
             "-----BEGIN CERTIFICATE-----\n" +
-            "MIIBrzCCAVWgAwIBAgIUZrPwF3PqWGDcrhVbe8rX4DwJmEUwCgYIKoEcz1UBg3Uw\n" +
-            "FDESMBAGA1UEAwwJY2Etc20yc20yMB4XDTIxMDkxMTIwMTUxNloXDTMxMDkwOTIw\n" +
-            "MTUxNlowHjEcMBoGA1UEAwwTaW50Y2Etc20yc20yLXNtMnNtMjBZMBMGByqGSM49\n" +
-            "AgEGCCqBHM9VAYItA0IABGLX296HPUZRxCUA0wto7flk/+Rim4CF+2r4wz9LOOVy\n" +
-            "XJK42DMPn1DaU5NSBtLmkPKuVcjBQE9QF6S44nu6j2SjezB5MB0GA1UdDgQWBBS9\n" +
-            "qmRtTUAzgbdQs00vEn2Opu9kQjAfBgNVHSMEGDAWgBQKYcEi1iAGgEhoipbvCEd1\n" +
-            "/zIBhDAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAWBgNVHSUBAf8E\n" +
-            "DDAKBggrBgEFBQcDCTAKBggqgRzPVQGDdQNIADBFAiBhZ2wnl2uyKbvU2dJL31F1\n" +
-            "6aSMTWpH3VqAhw7iPmXKPAIhAOuHJmkcFAv/QzIWHb0hs7WN/t3srddg/8JDd4ee\n" +
-            "Y7QW\n" +
+            "MIIBsDCCAVagAwIBAgIUIZz0xdu7Az5p0jJqRneq96NraFIwCgYIKoEcz1UBg3Uw\n" +
+            "EjEQMA4GA1UEAwwHdGxjcC1jYTAeFw0yMjAzMTAxMTA4MjlaFw0zMjAzMDcxMTA4\n" +
+            "MjlaMBUxEzARBgNVBAMMCnRsY3AtaW50Y2EwWTATBgcqhkjOPQIBBggqgRzPVQGC\n" +
+            "LQNCAAS/2B8sH0dPDeYEqxRzT3WIRF3sWSU9m4x0Mx30ZsaXGttBIUqrNHV96AED\n" +
+            "eI/Y58nNj/3+RYLYAtBTKsvQ5Mhho4GGMIGDMB0GA1UdDgQWBBTLZx047EpmXZh2\n" +
+            "TXFfR31DPlkH/TAfBgNVHSMEGDAWgBRaalNU3LeAKmtWwjf4GPKBoIGzTTAPBgNV\n" +
+            "HRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBvjAgBgNVHSUBAf8EFjAUBggrBgEF\n" +
+            "BQcDCQYIKwYBBQUHAwgwCgYIKoEcz1UBg3UDSAAwRQIhAJjq30tQ6434GZ4yFRFo\n" +
+            "ASaN8KjtXiih9BTZ7TEsf2E2AiB3ZIf+qlAB4hzs4NfRjhrlX172SApgPHxoHUxe\n" +
+            "gf3SXg==\n" +
             "-----END CERTIFICATE-----";
 
     // The CA private key.
     private static final String CA_KEY =
-            "MIGHAgEAMBMGByqGSM49AgEGCCqBHM9VAYItBG0wawIBAQQgKWfrthUXfdgXGGI9\n" +
-            "qBJO/uIvABG6E4gcq/vMeGldll+hRANCAARi19vehz1GUcQlANMLaO35ZP/kYpuA\n" +
-            "hftq+MM/SzjlclySuNgzD59Q2lOTUgbS5pDyrlXIwUBPUBekuOJ7uo9k";
+            "MIGHAgEAMBMGByqGSM49AgEGCCqBHM9VAYItBG0wawIBAQQgPWgd9v0oxNwV1/nL\n" +
+            "UVaZuqCxxJmSeyVzvaWgb4lxB3qhRANCAAS/2B8sH0dPDeYEqxRzT3WIRF3sWSU9\n" +
+            "m4x0Mx30ZsaXGttBIUqrNHV96AEDeI/Y58nNj/3+RYLYAtBTKsvQ5Mhh";
 
-    /* The end entity certificate with ID "custom-id".
+    /* The sign certificate.
      *
      * Certificate:
      *     Data:
      *         Version: 3 (0x2)
      *         Serial Number:
-     *             47:d5:15:57:b6:d3:34:d8:dc:c8:8e:1f:e6:37:0e:62:1f:b1:98:a7
+     *             4d:98:a4:22:6f:c7:f9:74:8d:56:c6:1c:7e:7e:12:b6:52:81:79
      *         Signature Algorithm: SM2-with-SM3
-     *         Issuer: CN = intca-sm2sm2-sm2sm2
+     *         Issuer: CN = tlcp-intca
      *         Validity
-     *             Not Before: Jan 20 04:36:34 2022 GMT
-     *             Not After : Jan 18 04:36:34 2032 GMT
+     *             Not Before: Apr  3 23:58:03 2022 GMT
+     *             Not After : Mar 31 23:58:03 2032 GMT
      *         Subject: CN = localhost
      *         Subject Public Key Info:
      *             Public Key Algorithm: id-ecPublicKey
      *                 Public-Key: (256 bit)
      *                 pub:
-     *                     04:99:43:82:0c:7a:2e:39:32:45:61:0f:24:85:8e:
-     *                     90:f5:d7:b3:33:e5:f2:6c:1f:cb:81:b6:5b:47:67:
-     *                     32:4f:a3:0d:e8:b0:d2:0e:1c:b7:4d:57:8d:e0:45:
-     *                     c8:28:e6:07:be:b8:9b:12:a0:bb:f2:d3:48:fd:fd:
-     *                     73:8a:b0:05:4e
+     *                     04:63:d7:44:3b:17:45:96:f4:d7:ff:a2:2c:1d:34:
+     *                     bf:81:6e:aa:2f:25:af:f0:5d:e0:75:1c:40:31:dd:
+     *                     8c:8d:29:8d:c0:fe:20:3e:2a:f8:97:36:31:d0:bb:
+     *                     65:6b:87:ad:6a:a6:26:20:05:05:aa:a4:c3:00:c6:
+     *                     79:62:18:9b:a5
      *                 ASN1 OID: SM2
      *         X509v3 extensions:
      *             X509v3 Subject Key Identifier:
-     *                 E5:C6:9A:5B:66:F4:49:EF:2D:A9:74:FF:BB:5B:15:A9:EE:7E:2E:6F
+     *                 DA:85:F0:6C:9D:E9:17:89:A6:48:3D:18:F5:8B:D1:50:BF:6A:13:59
      *             X509v3 Authority Key Identifier:
-     *                 keyid:BD:AA:64:6D:4D:40:33:81:B7:50:B3:4D:2F:12:7D:8E:A6:EF:64:42
+     *                 keyid:CB:67:1D:38:EC:4A:66:5D:98:76:4D:71:5F:47:7D:43:3E:59:07:FD
      *
+     *             X509v3 Basic Constraints: critical
+     *                 CA:FALSE
+     *             X509v3 Key Usage: critical
+     *                 Digital Signature, Certificate Sign, CRL Sign
      *     Signature Algorithm: SM2-with-SM3
-     *          30:45:02:21:00:e9:87:48:f5:05:67:7b:1f:4e:33:62:73:b7:
-     *          9c:06:2b:97:65:bf:29:66:dc:69:04:e9:53:fa:e2:60:db:63:
-     *          bb:02:20:43:3c:cd:5f:2b:e3:2a:e6:74:e9:a4:2d:d0:15:bf:
-     *          a2:36:1d:fd:a6:b4:df:f3:c1:88:71:2d:99:7e:c9:8d:1b
+     *          30:44:02:20:55:dc:e4:91:84:68:72:e9:40:4c:cc:e0:a4:8b:
+     *          ce:17:05:47:64:1b:e4:bb:15:b4:e2:8f:f3:b9:a9:b5:81:cd:
+     *          02:20:75:b2:50:7f:d1:49:79:a5:24:a1:bc:d7:e4:19:56:9e:
+     *          e2:40:a9:29:c1:c9:83:38:10:79:fb:2c:fa:2a:be:50
      */
-    private static final String EE =
+    private static final String SIGN_EE =
             "-----BEGIN CERTIFICATE-----\n" +
-            "MIIBdjCCARygAwIBAgIUR9UVV7bTNNjcyI4f5jcOYh+xmKcwCgYIKoEcz1UBg3Uw\n" +
-            "HjEcMBoGA1UEAwwTaW50Y2Etc20yc20yLXNtMnNtMjAeFw0yMjAxMjAwNDM2MzRa\n" +
-            "Fw0zMjAxMTgwNDM2MzRaMBQxEjAQBgNVBAMMCWxvY2FsaG9zdDBZMBMGByqGSM49\n" +
-            "AgEGCCqBHM9VAYItA0IABJlDggx6LjkyRWEPJIWOkPXXszPl8mwfy4G2W0dnMk+j\n" +
-            "Deiw0g4ct01XjeBFyCjmB764mxKgu/LTSP39c4qwBU6jQjBAMB0GA1UdDgQWBBTl\n" +
-            "xppbZvRJ7y2pdP+7WxWp7n4ubzAfBgNVHSMEGDAWgBS9qmRtTUAzgbdQs00vEn2O\n" +
-            "pu9kQjAKBggqgRzPVQGDdQNIADBFAiEA6YdI9QVnex9OM2Jzt5wGK5dlvylm3GkE\n" +
-            "6VP64mDbY7sCIEM8zV8r4yrmdOmkLdAVv6I2Hf2mtN/zwYhxLZl+yY0b\n" +
+            "MIIBiTCCATCgAwIBAgITTZikIm/H+XSNVsYcfn4StlKBeTAKBggqgRzPVQGDdTAV\n" +
+            "MRMwEQYDVQQDDAp0bGNwLWludGNhMB4XDTIyMDQwMzIzNTgwM1oXDTMyMDMzMTIz\n" +
+            "NTgwM1owFDESMBAGA1UEAwwJbG9jYWxob3N0MFkwEwYHKoZIzj0CAQYIKoEcz1UB\n" +
+            "gi0DQgAEY9dEOxdFlvTX/6IsHTS/gW6qLyWv8F3gdRxAMd2MjSmNwP4gPir4lzYx\n" +
+            "0Ltla4etaqYmIAUFqqTDAMZ5YhibpaNgMF4wHQYDVR0OBBYEFNqF8Gyd6ReJpkg9\n" +
+            "GPWL0VC/ahNZMB8GA1UdIwQYMBaAFMtnHTjsSmZdmHZNcV9HfUM+WQf9MAwGA1Ud\n" +
+            "EwEB/wQCMAAwDgYDVR0PAQH/BAQDAgGGMAoGCCqBHM9VAYN1A0cAMEQCIFXc5JGE\n" +
+            "aHLpQEzM4KSLzhcFR2Qb5LsVtOKP87mptYHNAiB1slB/0Ul5pSShvNfkGVae4kCp\n" +
+            "KcHJgzgQefss+iq+UA==\n" +
             "-----END CERTIFICATE-----";
 
-    private static final String EE_ID = "custom-id";
+    private static final String SIGN_EE_ID = "1234567812345678";
 
-    // The end entity private key.
-    private static final String EE_KEY =
-            "MIGHAgEAMBMGByqGSM49AgEGCCqBHM9VAYItBG0wawIBAQQgyfT1QME3uMIbjAWB\n" +
-            "A0dXvT6J6Jkwv5IAd20IAKgjatGhRANCAASZQ4IMei45MkVhDySFjpD117Mz5fJs\n" +
-            "H8uBtltHZzJPow3osNIOHLdNV43gRcgo5ge+uJsSoLvy00j9/XOKsAVO";
+    // The sign certificate private key.
+    private static final String SIGN_EE_KEY =
+            "MIGHAgEAMBMGByqGSM49AgEGCCqBHM9VAYItBG0wawIBAQQg5ITc2VNmxpeum3gN\n" +
+            "f2LUjGmdc3G45uGNDFeiRLYxdKmhRANCAARj10Q7F0WW9Nf/oiwdNL+BbqovJa/w\n" +
+            "XeB1HEAx3YyNKY3A/iA+KviXNjHQu2Vrh61qpiYgBQWqpMMAxnliGJul";
+
+    /* The enc certificate.
+     *
+     * Certificate:
+     *     Data:
+     *         Version: 3 (0x2)
+     *         Serial Number:
+     *             4d:98:a4:22:6f:c7:f9:74:8d:56:c6:1c:7e:7e:12:b6:52:81:78
+     *         Signature Algorithm: SM2-with-SM3
+     *         Issuer: CN = tlcp-intca
+     *         Validity
+     *             Not Before: Apr  3 23:57:39 2022 GMT
+     *             Not After : Mar 31 23:57:39 2032 GMT
+     *         Subject: CN = localhost
+     *         Subject Public Key Info:
+     *             Public Key Algorithm: id-ecPublicKey
+     *                 Public-Key: (256 bit)
+     *                 pub:
+     *                     04:ab:85:de:73:98:9e:44:01:23:2c:8c:f8:53:29:
+     *                     ae:f5:1e:66:f1:6d:25:1f:22:69:7d:b4:9b:89:86:
+     *                     1e:9a:65:09:7d:2c:df:fb:b8:41:ea:ff:c1:73:d0:
+     *                     f6:60:e0:f7:bd:73:2e:a6:be:c6:b5:c5:d5:13:01:
+     *                     1f:fe:2a:28:a6
+     *                 ASN1 OID: SM2
+     *         X509v3 extensions:
+     *             X509v3 Subject Key Identifier:
+     *                 64:35:36:FB:4E:6E:16:44:7E:21:A9:FE:2E:FF:BC:91:9B:ED:15:6D
+     *             X509v3 Authority Key Identifier:
+     *                 keyid:CB:67:1D:38:EC:4A:66:5D:98:76:4D:71:5F:47:7D:43:3E:59:07:FD
+     *
+     *             X509v3 Basic Constraints: critical
+     *                 CA:FALSE
+     *             X509v3 Key Usage: critical
+     *                 Key Encipherment, Data Encipherment, Key Agreement
+     *     Signature Algorithm: SM2-with-SM3
+     *          30:46:02:21:00:b2:1b:8d:78:a2:19:bd:39:63:5b:6c:24:69:
+     *          50:e2:85:b6:2b:a2:b8:15:b9:29:64:60:b2:07:f1:77:f1:70:
+     *          7e:02:21:00:be:d2:f1:d7:a8:a1:cd:f3:5e:25:06:c1:9f:5e:
+     *          ec:67:9b:4d:06:9b:39:84:34:07:c9:d1:b7:81:14:33:21:21
+     */
+    private static final String ENC_EE =
+            "-----BEGIN CERTIFICATE-----\n" +
+            "MIIBizCCATCgAwIBAgITTZikIm/H+XSNVsYcfn4StlKBeDAKBggqgRzPVQGDdTAV\n" +
+            "MRMwEQYDVQQDDAp0bGNwLWludGNhMB4XDTIyMDQwMzIzNTczOVoXDTMyMDMzMTIz\n" +
+            "NTczOVowFDESMBAGA1UEAwwJbG9jYWxob3N0MFkwEwYHKoZIzj0CAQYIKoEcz1UB\n" +
+            "gi0DQgAEq4Xec5ieRAEjLIz4Uymu9R5m8W0lHyJpfbSbiYYemmUJfSzf+7hB6v/B\n" +
+            "c9D2YOD3vXMupr7GtcXVEwEf/ioopqNgMF4wHQYDVR0OBBYEFGQ1NvtObhZEfiGp\n" +
+            "/i7/vJGb7RVtMB8GA1UdIwQYMBaAFMtnHTjsSmZdmHZNcV9HfUM+WQf9MAwGA1Ud\n" +
+            "EwEB/wQCMAAwDgYDVR0PAQH/BAQDAgM4MAoGCCqBHM9VAYN1A0kAMEYCIQCyG414\n" +
+            "ohm9OWNbbCRpUOKFtiuiuBW5KWRgsgfxd/FwfgIhAL7S8deooc3zXiUGwZ9e7Geb\n" +
+            "TQabOYQ0B8nRt4EUMyEh\n" +
+            "-----END CERTIFICATE-----";
+
+    private static final String ENC_EE_ID = "1234567812345678";
+
+    // The enc certificate private key.
+    private static final String ENC_EE_KEY =
+            "MIGHAgEAMBMGByqGSM49AgEGCCqBHM9VAYItBG0wawIBAQQg+4a+CrNbNAyJ75Rm\n" +
+            "fN97JHcsbL9lqM5CWBFv0ooD4b2hRANCAASrhd5zmJ5EASMsjPhTKa71HmbxbSUf\n" +
+            "Iml9tJuJhh6aZQl9LN/7uEHq/8Fz0PZg4Pe9cy6mvsa1xdUTAR/+Kiim";
 
     private static final String PASSWORD = "password";
 
     @Test
-    public void tlsDemo() throws Exception {
+    public void tlcpDemo() throws Exception {
         // Output debug info.
 //        System.setProperty("com.tencent.kona.ssl.debug", "all");
-
-        // Just use SM2 curve for key share
-        System.setProperty("com.tencent.kona.ssl.namedGroups", "curveSM2");
 
         // Add providers.
         TestUtils.addProviders();
@@ -195,7 +256,7 @@ public class TLSWithGRPCDemo {
         return new JdkSslContext(
                 createContext(),
                 isClient,
-                Arrays.asList("TLS_SM4_GCM_SM3"),
+                Arrays.asList("TLCP_ECC_SM4_CBC_SM3"),
                 new AllAllowedCipherSuiteFilter(),
                 new ApplicationProtocolConfig(
                         ApplicationProtocolConfig.Protocol.ALPN,
@@ -203,7 +264,7 @@ public class TLSWithGRPCDemo {
                         ApplicationProtocolConfig.SelectedListenerFailureBehavior.FATAL_ALERT,
                         "h2", "HTTP/1.1"),
                 ClientAuth.NONE,
-                new String[] {"TLSv1.3"},
+                new String[] {"TLCPv1.1"},
                 false);
     }
 
@@ -212,11 +273,13 @@ public class TLSWithGRPCDemo {
         TrustManagerFactory tmf = SSLInsts.getTrustManagerFactory("PKIX");
         tmf.init(trustStore);
 
-        KeyStore keyStore = createKeyStore(EE, EE_ID, EE_KEY);
+        KeyStore keyStore = createKeyStore(
+                SIGN_EE, SIGN_EE_ID, SIGN_EE_KEY,
+                ENC_EE, ENC_EE_ID, ENC_EE_KEY);
         KeyManagerFactory kmf = SSLInsts.getKeyManagerFactory("NewSunX509");
         kmf.init(keyStore, PASSWORD.toCharArray());
 
-        SSLContext context = SSLInsts.getSSLContext("TLS");
+        SSLContext context = SSLInsts.getSSLContext("TLCPv1.1");
         context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
         return context;
     }
@@ -225,28 +288,33 @@ public class TLSWithGRPCDemo {
             throws Exception {
         KeyStore trustStore = PKIXInsts.getKeyStore("PKCS12");
         trustStore.load(null, null);
-        trustStore.setCertificateEntry("tls-trust-demo", loadCert(caStr, caId));
+        trustStore.setCertificateEntry("tlcp-trust-demo", loadCert(caStr, caId));
         return trustStore;
     }
 
     private static KeyStore createKeyStore(
-            String eeStr, String eeId, String eeKeyStr)
+            String signEeStr, String signEeId, String signEeKeyStr,
+            String encEeStr, String encEeId, String encEeKeyStr)
             throws Exception {
         KeyStore keyStore = PKIXInsts.getKeyStore("PKCS12");
         keyStore.load(null, null);
 
-        PrivateKey privateKey = loadPrivateKey(eeKeyStr);
-        keyStore.setKeyEntry("tls-ee-demo",
-                privateKey,
+        keyStore.setKeyEntry("tlcp-sign-ee-demo",
+                loadPrivateKey(signEeKeyStr),
                 PASSWORD.toCharArray(),
-                new Certificate[] { loadCert(eeStr, eeId) } );
+                new Certificate[] { loadCert(signEeStr, signEeId) } );
+        keyStore.setKeyEntry("tlcp-enc-ee-demo",
+                loadPrivateKey(encEeKeyStr),
+                PASSWORD.toCharArray(),
+                new Certificate[] { loadCert(encEeStr, encEeId) } );
 
         return keyStore;
     }
 
     private static X509Certificate loadCert(String certPEM, String id)
             throws Exception {
-        CertificateFactory certFactory = PKIXInsts.getCertificateFactory("X.509");
+        CertificateFactory certFactory = PKIXInsts.getCertificateFactory(
+                "X.509");
         X509Certificate x509Cert = (X509Certificate) certFactory.generateCertificate(
                 new ByteArrayInputStream(certPEM.getBytes()));
 
