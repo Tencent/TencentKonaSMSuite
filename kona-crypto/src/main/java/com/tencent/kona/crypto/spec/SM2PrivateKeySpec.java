dd567a230a472939aaf042744d91d312a2453de5
John Jiang <johnsjiang@tencent.com>
2023-10-12 15:34:05 +0800

    TKSS-457: SM2 KeySpecs should not copy a part of a byte array as keys

--- kona-crypto/src/main/java/com/tencent/kona/crypto/spec/SM2PrivateKeySpec.java
+++ kona-crypto/src/main/java/com/tencent/kona/crypto/spec/SM2PrivateKeySpec.java
@@ -20,23 +20,19 @@
 
 package com.tencent.kona.crypto.spec;
 
-import com.tencent.kona.crypto.CryptoUtils;
-
 import java.math.BigInteger;
 import java.security.spec.ECPrivateKeySpec;
 
+/**
+ * A SM2 private key with the specified value.
+ */
 public class SM2PrivateKeySpec extends ECPrivateKeySpec {
 
-    public SM2PrivateKeySpec(byte[] key, int offset, int length) {
-        super(new BigInteger(CryptoUtils.copy(key, offset, length)),
-                SM2ParameterSpec.instance());
+    public SM2PrivateKeySpec(BigInteger s) {
+        super(s, SM2ParameterSpec.instance());
     }
 
-    public SM2PrivateKeySpec(byte[] key) {
-        this(key, 0, key.length);
-    }
-
-    public SM2PrivateKeySpec(byte[] key, int offset) {
-        this(key, offset, key.length - offset);
+    public SM2PrivateKeySpec(byte[] sKey) {
+        this(new BigInteger(1, sKey));
     }
 }
