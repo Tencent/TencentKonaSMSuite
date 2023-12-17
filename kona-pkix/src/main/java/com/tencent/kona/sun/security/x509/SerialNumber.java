156416201f156a4f5ffc87bbab40d32c6377da89
John Jiang <johnsjiang@tencent.com>
2023-12-18 05:18:53 +0800

    TKSS-597: Backport JDK-8296787: Unify debug printing format of X.509 cert serial numbers

--- kona-pkix/src/main/java/com/tencent/kona/sun/security/x509/SerialNumber.java
+++ kona-pkix/src/main/java/com/tencent/kona/sun/security/x509/SerialNumber.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -104,7 +104,7 @@ public class SerialNumber {
      * Return the SerialNumber as user readable string.
      */
     public String toString() {
-        return "SerialNumber: [" + Debug.toHexString(serialNum) + ']';
+        return "SerialNumber: " + Debug.toString(serialNum);
     }
 
     /**
