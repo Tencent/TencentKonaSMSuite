739c499433525901c44464374ae5066991b62ad7
John Jiang <johnsjiang@tencent.com>
2023-12-19 10:43:12 +0800

    TKSS-604: TLS13_SM_ID would be private

--- kona-ssl/src/main/java/com/tencent/kona/sun/security/ssl/Utilities.java
+++ kona-ssl/src/main/java/com/tencent/kona/sun/security/ssl/Utilities.java
@@ -25,10 +25,6 @@ import java.nio.charset.StandardCharsets;
 
 public class Utilities extends com.tencent.kona.sun.security.util.Utilities {
 
-    // The ID used by TLS 1.3 handshaking with signature scheme SM3withSM2.
-    public static final byte[] TLS13_SM_ID
-            = "TLSv1.3+GM+Cipher+Suite".getBytes(StandardCharsets.ISO_8859_1);
-
     public static final boolean SUPPORT_ALPN = supportALPN();
 
     private static boolean supportALPN() {
