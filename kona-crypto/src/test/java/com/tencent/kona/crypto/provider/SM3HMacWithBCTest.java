2598b38af9fcc3da05a9c008d525a61c8167cf54
John Jiang <johnsjiang@tencent.com>
2024-09-02 14:07:34 +0800

    TKSS-846: SM3HMacWithBCTest uses Kona SM3HMac twice

--- kona-crypto/src/test/java/com/tencent/kona/crypto/provider/SM3HMacWithBCTest.java
+++ kona-crypto/src/test/java/com/tencent/kona/crypto/provider/SM3HMacWithBCTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022, 2023, THL A29 Limited, a Tencent company. All rights reserved.
+ * Copyright (C) 2022, 2024, THL A29 Limited, a Tencent company. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -58,7 +58,7 @@ public class SM3HMacWithBCTest {
         sm3HMacBC.init(secretKey);
 
         byte[] mac = sm3HMac.doFinal(message);
-        byte[] macBC = sm3HMac.doFinal(message);
+        byte[] macBC = sm3HMacBC.doFinal(message);
 
         Assertions.assertArrayEquals(macBC, mac);
     }
