065fcf936ab9797f3f856c552c7ef93d6e1889ae
John Jiang <johnsjiang@tencent.com>
2024-09-02 16:22:47 +0800

    TKSS-848: SM3HMacPerfTest uses Kona SM3HMac twice

--- kona-crypto/src/jmh/java/com/tencent/kona/crypto/perf/SM3HMacPerfTest.java
+++ kona-crypto/src/jmh/java/com/tencent/kona/crypto/perf/SM3HMacPerfTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022, 2023, THL A29 Limited, a Tencent company. All rights reserved.
+ * Copyright (C) 2022, 2024, THL A29 Limited, a Tencent company. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -83,7 +83,7 @@ public class SM3HMacPerfTest {
 
         @Setup(Level.Trial)
         public void setup() throws Exception {
-            mac = Mac.getInstance("HMACSM3", PROVIDER);
+            mac = Mac.getInstance("HMACSM3", "BC");
             mac.init(SECRET_KEY);
         }
     }
