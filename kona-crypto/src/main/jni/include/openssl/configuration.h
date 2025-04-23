81258ecd6d1a4e74fb257fade1c3af7fb34ce909
johnsjiang <johnsjiang@tencent.com>
2025-04-23 14:09:25 +0800

    TKSS-1165: Upgrade OpenSSL to 3.5.0

--- kona-crypto/src/main/jni/include/openssl/configuration.h
+++ kona-crypto/src/main/jni/include/openssl/configuration.h
@@ -30,7 +30,7 @@ extern "C" {
 # ifndef OPENSSL_SYS_MACOSX
 #  define OPENSSL_SYS_MACOSX 1
 # endif
-# define OPENSSL_CONFIGURED_API 30400
+# define OPENSSL_CONFIGURED_API 30500
 # ifndef OPENSSL_RAND_SEED_OS
 #  define OPENSSL_RAND_SEED_OS
 # endif
@@ -73,6 +73,9 @@ extern "C" {
 # ifndef OPENSSL_NO_EXTERNAL_TESTS
 #  define OPENSSL_NO_EXTERNAL_TESTS
 # endif
+# ifndef OPENSSL_NO_FIPS_JITTER
+#  define OPENSSL_NO_FIPS_JITTER
+# endif
 # ifndef OPENSSL_NO_FIPS_POST
 #  define OPENSSL_NO_FIPS_POST
 # endif
@@ -87,6 +90,9 @@ extern "C" {
 # endif
 # ifndef OPENSSL_NO_H3DEMO
 #  define OPENSSL_NO_H3DEMO
+# endif
+# ifndef OPENSSL_NO_HQINTEROP
+#  define OPENSSL_NO_HQINTEROP
 # endif
 # ifndef OPENSSL_NO_JITTER
 #  define OPENSSL_NO_JITTER
@@ -114,6 +120,9 @@ extern "C" {
 # endif
 # ifndef OPENSSL_NO_SSL3_METHOD
 #  define OPENSSL_NO_SSL3_METHOD
+# endif
+# ifndef OPENSSL_NO_SSLKEYLOG
+#  define OPENSSL_NO_SSLKEYLOG
 # endif
 # ifndef OPENSSL_NO_TFO
 #  define OPENSSL_NO_TFO
