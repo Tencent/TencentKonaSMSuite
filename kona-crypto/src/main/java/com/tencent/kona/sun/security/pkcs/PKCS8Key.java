3af3808b3492095e40169f2710508c866b06f92b
johnsjiang <johnsjiang@tencent.com>
2025-06-11 18:35:53 +0800

    TKSS-1180: Backport JDK-8358316: PKCS8Key.getEncoded() can throw NPE after JDK-8298420

--- kona-crypto/src/main/java/com/tencent/kona/sun/security/pkcs/PKCS8Key.java
+++ kona-crypto/src/main/java/com/tencent/kona/sun/security/pkcs/PKCS8Key.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -204,7 +204,8 @@ public class PKCS8Key implements PrivateKey, InternalPrivateKey {
      * or {@code null} if an encoding error occurs.
      */
     public byte[] getEncoded() {
-        return getEncodedInternal().clone();
+        byte[] b = getEncodedInternal();
+        return (b != null) ? b.clone() : null;
     }
 
     /**
