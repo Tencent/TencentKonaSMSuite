465901d19d43ed2b353cca261d2e6756a6bd19de
johnsjiang <johnsjiang@tencent.com>
2025-07-15 15:36:40 +0800

    Backport JDK-8156715: TrustStoreManager does not buffer keystore input stream

--- kona-ssl/src/main/java/com/tencent/kona/sun/security/ssl/TrustStoreManager.java
+++ kona-ssl/src/main/java/com/tencent/kona/sun/security/ssl/TrustStoreManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -388,9 +388,10 @@ final class TrustStoreManager {
             }
 
             if (!"NONE".equals(descriptor.storeName)) {
-                try (@SuppressWarnings("removal") FileInputStream fis = AccessController.doPrivileged(
-                        new OpenFileInputStreamAction(descriptor.storeFile))) {
-                    ks.load(fis, password);
+                try (BufferedInputStream bis =
+                        new BufferedInputStream(
+                                new FileInputStream(descriptor.storeFile))) {
+                    ks.load(bis, password);
                 } catch (FileNotFoundException fnfe) {
                     // No file available, no KeyStore available.
                     if (SSLLogger.isOn && SSLLogger.isOn("trustmanager")) {
